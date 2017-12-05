2cbc4a1a61b487b17f58e90c62c771b2c8125236
a-staab <astaab@gmail.com>
2017-12-04 19:34:05 -0800

    Adding better_binary_search.py

---
+++ better_binary_search.py
@@ -0,0 +1,55 @@
+
+"""
+Given a sorted array of integers, return the index of a target element in the
+array. If the element is not in the array, return -1. Assume there are no
+duplicates in the array.
+
+>>> get_index([0, 20, 34, 60, 79, 80], 31)
+-1
+
+>>> get_index([0, 20, 34, 60, 79, 80], 100)
+-1
+
+>>> get_index([20, 34, 60, 79, 80], 0)
+-1
+
+>>> get_index([0, 20, 34, 60, 79, 80], 0)
+0
+
+>>> get_index([0, 20, 34, 60, 79, 80], 20)
+1
+
+>>> get_index([0, 20, 34, 60, 79, 80], 34)
+2
+
+>>> get_index([0, 20, 34, 60, 79, 80], 60)
+3
+
+>>> get_index([0, 20, 34, 60, 79, 80], 79)
+4
+
+>>> get_index([0, 20, 34, 60, 79, 80], 80)
+5
+
+"""
+
+
+def get_index(arr, target):
+
+    lower = 0
+    upper = len(arr) - 1
+    result = -1
+
+    while lower <= upper and result == -1:
+        midpoint = lower + (upper - lower)/2
+
+        if arr[midpoint] == target:
+            result = midpoint
+
+        elif arr[midpoint] > target:
+            upper = midpoint - 1
+
+        elif arr[midpoint] < target:
+            lower = midpoint + 1
+
+    return result
