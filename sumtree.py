f6a6ffe6de43bcbcba7311bbf29dbe92774035d1
a-staab <astaab@gmail.com>
2017-12-26 18:43:22 -0800

    Adding sumtree.py

---
+++ sumtree.py
@@ -0,0 +1,21 @@
+"""
+Check if a given binary tree is SumTree - GeeksforGeeks
+
+Write a function that returns True if the given binary tree is a SumTree or 
+False if it is not. A SumTree is a binary tree where the value of each node is 
+equal to the sum of the nodes present in its left and right subtrees. An empty
+tree is a SumTree, and the sum of an empty tree is considered to be 0. A leaf 
+node is also considered a SumTree.
+
+Example:
+
+        26
+       /  \
+      10   3
+     /  \   \
+    4    6   3
+
+"""
+
+
+
