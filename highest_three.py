7c7cda48c0dcb20f3f9949f075352313b55cef4c
a-staab <astaab@gmail.com>
2017-12-29 08:41:02 -0800

    Wrote docstring, two tests

---
+++ highest_three.py
@@ -0,0 +1,14 @@
+"""
+Find the largest three elements in an array - "Geeks for Geeks"
+
+Given an array with all distinct elements, find the largest three elements.
+Expected time complexity is O(n), and extra space is O(1).
+
+>>> get_top_three([10, 4, 3, 50, 23, 90])
+90, 50, 23
+
+>>> get_top_three([5, 22, 0, -1, 20, 6])
+22, 20, 6
+
+"""
+
